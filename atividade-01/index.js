681a7dd800ef9c23cc6234a3621abec1ed713411
JVlCTOR <joaovictorresende59@gmail.com>
2023-10-09 18:31:59 -0300

    atulizado

--- atividade-01/index.js
+++ atividade-01/index.js
@@ -5,5 +5,5 @@ console.log(frase.toUpperCase());
 //Os metodos toUpperCase/toLowerCase transforma strings em maiusculo/minuscula. estes metodos é muito usado no dia a dia para exibir informação formatada para o usuário e também para validação de dados principalmente em formularios onde você pode converter automaticamente o texto inserido pelo usuário em maiusculas/minusculo para garantir que os dados sejam consistentes e facilmente comparaveis
 console.log(frase.toLowerCase());
 
-// O metodo split é uma função util no dia a dia da programação quando você precisa dividir uma string em partes com base em um caractere, sendo usado analise de texto,processamento de dados de entrada e manipulação de caminhos de arquivo;
-console.log(frase.split(''));
+// O metodo split é uma função util no dia a dia da programação quando você precisa transforma uma string em array, sendo usado analise de texto,processamento de dados de entrada e manipulação de caminhos de arquivo;
+console.log(frase.split(""));
